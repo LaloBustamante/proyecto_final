88a7e653a0d850a382f88b22a132f7b1c5396e53
eduardobustamante <eduardobustamanteblanco@gmail.com>
2025-01-07 18:21:04 -0600

    Plan de trabajo Jupyther Notebook

--- proyecto_final.py
+++ proyecto_final.py
@@ -1,4 +1,50 @@
 '''
+# Condiciones de la asignación principal
+
+Al operador de telecomunicaciones Interconnect le gustaría poder pronosticar su tasa de cancelación de clientes. Si se descubre que un usuario o usuaria planea irse, se le ofrecerán códigos promocionales y opciones de planes especiales. El equipo de marketing de Interconnect ha recopilado algunos de los datos personales de sus clientes, incluyendo información sobre sus planes y contratos.
+
+### Servicios de Interconnect
+
+Interconnect proporciona principalmente dos tipos de servicios:
+
+1. Comunicación por teléfono fijo. El teléfono se puede conectar a varias líneas de manera simultánea.
+2. Internet. La red se puede configurar a través de una línea telefónica (DSL, *línea de abonado digital*) o a través de un cable de fibra óptica.
+
+Algunos otros servicios que ofrece la empresa incluyen:
+
+- Seguridad en Internet: software antivirus (*ProtecciónDeDispositivo*) y un bloqueador de sitios web maliciosos (*SeguridadEnLínea*).
+- Una línea de soporte técnico (*SoporteTécnico*).
+- Almacenamiento de archivos en la nube y backup de datos (*BackupOnline*).
+- Streaming de TV (*StreamingTV*) y directorio de películas (*StreamingPelículas*)
+
+La clientela puede elegir entre un pago mensual o firmar un contrato de 1 o 2 años. Puede utilizar varios métodos de pago y recibir una factura electrónica después de una transacción.
+
+### Descripción de los datos
+
+Los datos consisten en archivos obtenidos de diferentes fuentes:
+
+- `contract.csv` — información del contrato;
+- `personal.csv` — datos personales del cliente;
+- `internet.csv` — información sobre los servicios de Internet;
+- `phone.csv` — información sobre los servicios telefónicos.
+
+En cada archivo, la columna `customerID` (ID de cliente) contiene un código único asignado a cada cliente. La información del contrato es válida a partir del 1 de febrero de 2020.
+
+### Datos
+
+[final_provider.zip](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/94210e31-fd3d-451b-a350-4a8476756413/final_provider.zip)
+
+Los datos también se encuentran en la plataforma, en la carpeta `/datasets/final_provider/`.
+
+
+# 1. Plan de trabajo
+
+Deberás realizar un análisis exploratorio de datos. Al final de *Jupyter Notebook*, escribe:
+
+- Una lista de preguntas aclaratorias.
+- Un plan aproximado para resolver la tarea, que especifica de 3 a 5 pasos básicos y los explica en uno o dos enunciados
+
+El líder del equipo revisará tus preguntas y plan de trabajo. Las preguntas serán respondidas durante una videollamada. El código será revisado por el líder del equipo solo si hay algunas dudas.
 
 '''
 
